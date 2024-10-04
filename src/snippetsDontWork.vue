f4f6644eb25a9425e40e9e6a273ef8e7f2b70965
Patrick Raedler <code@readpato.dev>
2024-10-04 21:47:10 +0200

    chore: init

---
+++ src/snippetsDontWork.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const doVueSnippetsWork = ref(false);
+const doTsSnippetsWork = ref(true);
+</script>
+
+<template>
+    <div>
+        <h1>Do Vue snippets work? {{ doVueSnippetsWork ? "Yes" : "No" }}</h1>
+        <h1>Do TS snippets work? {{ doTsSnippetsWork ? "Yes" : "No" }}</h1>
+    </div>
+</template>
---
+++ src/snippetsWork.ts
@@ -0,0 +1,4 @@
+export interface SnippetsWork {
+  foo: string;
+  bar: string;
+}
